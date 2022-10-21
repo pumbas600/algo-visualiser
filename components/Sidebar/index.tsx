a8ca9388ab58ecabf6e30b825c069a220e5e487e
pumbas600 <joshjeffers600@gmail.com>
2022-10-22 01:12:11 +1300

    feat: :sparkles: Render sidebar links

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -1,23 +1,23 @@
 import Box from '@mui/material/Box';
-import AppBar, { AppBarProps } from '@mui/material/AppBar';
 import Drawer from '@mui/material/Drawer';
 import { CSSObject, styled, Theme, useTheme } from '@mui/material/styles';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import Typography from '@mui/material/Typography';
 import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import DataArrayIcon from '@mui/icons-material/DataArray';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
-import { useState } from 'react';
+import SearchIcon from '@mui/icons-material/Search';
+import SortIcon from '@mui/icons-material/Sort';
+import TimelineIcon from '@mui/icons-material/Timeline';
+import { ReactNode, useCallback, useState } from 'react';
 import { Stack } from '@mui/material';
+import Link from 'next/link';
+import { blue, green, purple, red } from '@mui/material/colors';
 
 const drawerWidth = 240;
 
@@ -65,6 +65,44 @@ const CustomDrawer = styled(Drawer, { shouldForwardProp: (prop) => prop !== 'ope
   }),
 }));
 
+export interface SidebarLink {
+  icon?: ReactNode;
+  href: string;
+  title: string;
+  colour: string;
+}
+
+export interface SidebarProps {
+  links: SidebarLink[];
+}
+
+const SidebarLinks: SidebarLink[] = [
+  {
+    icon: <DataArrayIcon />,
+    title: 'Data Structures',
+    colour: green[500],
+    href: '/data-structures',
+  },
+  {
+    icon: <SortIcon />,
+    title: 'Sorting',
+    colour: blue[500],
+    href: '/sorting',
+  },
+  {
+    icon: <SearchIcon />,
+    title: 'Searching',
+    colour: purple[500],
+    href: '/searching',
+  },
+  {
+    icon: <TimelineIcon />,
+    title: 'Graph Traversal',
+    colour: red[500],
+    href: '/graph-traversal',
+  },
+];
+
 export default function Sidebar() {
   const theme = useTheme();
   const [open, setOpen] = useState(false);
@@ -76,6 +114,38 @@ export default function Sidebar() {
   const handleDrawerClose = () => {
     setOpen(false);
   };
+
+  const renderLinks = useCallback(() => {
+    return SidebarLinks.map((link) => (
+      <Link key={link.title} href={link.href}>
+        <ListItem disablePadding sx={{ display: 'block' }}>
+          <ListItemButton
+            sx={{
+              minHeight: 48,
+              justifyContent: open ? 'initial' : 'center',
+              px: 2.5,
+            }}
+          >
+            <ListItemIcon
+              sx={{
+                minWidth: 0,
+                mr: open ? 3 : 'auto',
+                justifyContent: 'center',
+                color: link.colour,
+              }}
+            >
+              {link.icon}
+            </ListItemIcon>
+            <ListItemText
+              primary={link.title}
+              primaryTypographyProps={{ color: link.colour, fontWeight: 'bold' }}
+              sx={{ opacity: open ? 1 : 0 }}
+            />
+          </ListItemButton>
+        </ListItem>
+      </Link>
+    ));
+  }, [open]);
 
   return (
     <Box>
@@ -93,28 +163,7 @@ export default function Sidebar() {
         </DrawerHeader>
         <Divider />
         <Stack gap={3} height="100%">
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem key={text} disablePadding sx={{ display: 'block' }}>
-              <ListItemButton
-                sx={{
-                  minHeight: 48,
-                  justifyContent: open ? 'initial' : 'center',
-                  px: 2.5,
-                }}
-              >
-                <ListItemIcon
-                  sx={{
-                    minWidth: 0,
-                    mr: open ? 3 : 'auto',
-                    justifyContent: 'center',
-                  }}
-                >
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
-              </ListItemButton>
-            </ListItem>
-          ))}
+          {renderLinks()}
         </Stack>
       </CustomDrawer>
     </Box>
