39db7cdcbf8137be88c544805b6ff5d5d395513c
pumbas600 <joshjeffers600@gmail.com>
2022-10-23 01:35:28 +1300

    feat: :sparkles: Create function for grouping headers

--- components/ContentLayout/index.tsx
+++ components/ContentLayout/index.tsx
@@ -3,15 +3,20 @@ import React, { Children, cloneElement, ReactElement, ReactNode, useCallback } f
 import { getChildren, isComponent } from '../Utilities';
 import Heading, { HeadingProps } from './Heading';
 
+interface HeadingGroup {
+  heading: HeadingProps | null;
+  subHeadings: HeadingProps[];
+}
+
 const ContentLayout = ({ children }: { children?: ReactNode }) => {
-  const getId = useCallback((title: string) => {
-    return title.replaceAll(/\s+/g, '-').toLowerCase();
+  const getId = useCallback((props: HeadingProps): string => {
+    return props.id ?? props.title.replaceAll(/\s+/g, '-').toLowerCase();
   }, []);
 
   const renderChildren = useCallback((): ReactNode => {
     return Children.map(children, (c) => {
       if (isComponent(Heading, c) && !c.props.id) {
-        const id = getId(c.props.title);
+        const id = getId(c.props);
         return cloneElement(c, { ...c.props, id });
       }
 
@@ -19,11 +24,46 @@ const ContentLayout = ({ children }: { children?: ReactNode }) => {
     });
   }, [children, getId]);
 
-  const getChildHeadings = useCallback((): ReactElement<HeadingProps>[] => {
+  const getChildHeadings = useCallback((): HeadingProps[] => {
     return getChildren(children)
       .filter((c) => c.type === Heading)
-      .map((c) => c as ReactElement<HeadingProps>);
+      .map((c) => c.props as HeadingProps);
   }, [children]);
+
+  const getHeadingGroups = useCallback((): HeadingGroup[] => {
+    const headings = getChildHeadings();
+    const groups: HeadingGroup[] = [];
+    const emptyGroup = {
+      heading: null,
+      subHeadings: [],
+    };
+
+    let currentGroup: HeadingGroup = emptyGroup;
+
+    headings.forEach((heading) => {
+      if (heading.subHeading) {
+        currentGroup.subHeadings.push(heading);
+      } else {
+        if (currentGroup !== emptyGroup) {
+          groups.push(currentGroup);
+          currentGroup = emptyGroup;
+        }
+        currentGroup.heading = heading;
+      }
+    });
+
+    if (currentGroup !== emptyGroup) {
+      groups.push(currentGroup);
+    }
+
+    return groups;
+  }, [getChildHeadings]);
+
+  const renderContent = useCallback((): ReactNode => {
+    const groups = getHeadingGroups();
+
+    return null;
+  }, [getHeadingGroups]);
 
   return (
     <Stack mx={8} my={4} spacing={1}>
