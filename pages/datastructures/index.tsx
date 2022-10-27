de4c36d31982416733f28dc5452eed7432a680f6
pumbas600 <joshjeffers600@gmail.com>
2022-10-27 16:59:21 +1300

    refactor: :recycle: Remove testing content from data structures page

--- pages/datastructures/index.tsx
+++ pages/datastructures/index.tsx
@@ -1,21 +1,7 @@
-import { Box, Typography } from '@mui/material';
 import ContentLayout from '../../components/ContentLayout';
-import Heading from '../../components/ContentLayout/Heading';
 
 const DataStructures = () => {
-  return (
-    <ContentLayout>
-      <Heading title="This is a heading" />
-      Hi!
-      <Box height="600px" width="100%" bgcolor="red">
-        Another child
-      </Box>
-      <Heading title={'This is a subheading'} subHeading />
-      <Box height="600px" width="100%" bgcolor="red" />
-      <Heading title={'One more Heading'} />
-      <Heading title={'One more subheading'} subHeading />
-    </ContentLayout>
-  );
+  return <ContentLayout></ContentLayout>;
 };
 
 export default DataStructures;
