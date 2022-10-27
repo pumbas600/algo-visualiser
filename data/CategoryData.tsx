72e7da6cad33dbf85a55065af521407bb61aea08
pumbas600 <joshjeffers600@gmail.com>
2022-10-27 17:56:00 +1300

    feat: :sparkles: Add subcategories

--- data/CategoryData.tsx
+++ data/CategoryData.tsx
@@ -1,4 +1,4 @@
-import { blue, green, purple, red } from '@mui/material/colors';
+import { blue, green, orange, purple, red } from '@mui/material/colors';
 import { ReactNode } from 'react';
 import DataArrayIcon from '@mui/icons-material/DataArray';
 import TimelineIcon from '@mui/icons-material/Timeline';
@@ -11,6 +11,12 @@ export interface CategoryData {
   colour: string;
   title: string;
   href: string;
+  subCategories: SubCategoryData[];
+}
+
+export interface SubCategoryData {
+  href: string;
+  title: string;
 }
 
 export const homeCategory: CategoryData = {
@@ -18,6 +24,7 @@ export const homeCategory: CategoryData = {
   colour: blue[500],
   title: 'Home',
   href: '/',
+  subCategories: [],
 };
 
 export const sidebarCategories: CategoryData[] = [
@@ -26,24 +33,132 @@ export const sidebarCategories: CategoryData[] = [
     title: 'Data Structures',
     colour: green[500],
     href: '/datastructures',
+    subCategories: [
+      {
+        title: 'Array',
+        href: '/array',
+      },
+      {
+        title: 'Linked List',
+        href: '/linkedlist',
+      },
+      {
+        title: 'Queue',
+        href: '/queue',
+      },
+      {
+        title: 'Stack',
+        href: '/stack',
+      },
+      {
+        title: 'Heap',
+        href: '/heap',
+      },
+      {
+        title: 'Binary Tree',
+        href: '/binarytree',
+      },
+    ],
   },
   {
     icon: <SortIcon />,
     title: 'Sorting',
     colour: blue[500],
     href: '/sorting',
+    subCategories: [
+      {
+        title: 'Selection Sort',
+        href: 'selection',
+      },
+      {
+        title: 'Insertion Sort',
+        href: '/insertion',
+      },
+      {
+        title: 'Shell Sort',
+        href: '/shell',
+      },
+      {
+        title: 'Merge Sort',
+        href: '/merge',
+      },
+      {
+        title: 'Quick Sort',
+        href: '/quick',
+      },
+      {
+        title: 'Heap Sort',
+        href: '/heap',
+      },
+    ],
   },
   {
     icon: <SearchIcon />,
     title: 'Searching',
     colour: purple[500],
     href: '/searching',
+    subCategories: [
+      {
+        title: 'Binary Search',
+        href: 'binarysearch',
+      },
+      {
+        title: 'Quick Select',
+        href: 'quickselect',
+      },
+    ],
   },
   {
     icon: <TimelineIcon />,
     title: 'Graph Traversal',
     colour: red[500],
     href: '/graphtraversal',
+    subCategories: [
+      {
+        title: 'Depth-first Search',
+        href: 'dfs',
+      },
+      {
+        title: 'Breadth-first Search',
+        href: 'bfs',
+      },
+      {
+        title: 'Priority-first Search',
+        href: 'pfs',
+      },
+      {
+        title: 'Topological Sorting',
+        href: 'topologicalsorting',
+      },
+    ],
+  },
+  {
+    icon: <TimelineIcon />,
+    title: 'Minimum Paths',
+    colour: orange[500],
+    href: '/minimumpaths',
+    subCategories: [
+      {
+        title: 'Dijkstra',
+        href: 'dijkstra',
+      },
+      {
+        title: 'Bellman-Ford',
+        href: 'bellmanford',
+      },
+      {
+        title: "Floyd's",
+        href: 'floyd',
+      },
+      {
+        title: "Prim's",
+        href: 'prim',
+      },
+      {
+        title: "Kruskal's",
+        href: 'kruskal',
+      },
+    ],
   },
 ];
 
